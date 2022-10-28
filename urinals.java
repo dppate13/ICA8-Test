4ecc9d01904f64b48a4a6b3bad5898b55280dcf3
dppate13 <dppate13@asu.edu>
2022-10-27 19:27:26 -0700

    update countUriinal

--- urinals.java
+++ urinals.java
@@ -14,22 +14,29 @@ public class urinals {
     int countUrinals(String str){
         int count = 0;
 
-        for(int i =0; i<str.length();i++){
+        if(goodString(str)){
+            for(int i =0; i<str.length();i++){
 
-            if(str.charAt(i) == '0'){
-                if(str.charAt(i+1) == '0' && str.charAt(i-1) == '0'){
-                    count++;
+                if(str.charAt(i) == '0'){
+                    if((i-1<0 || str.charAt(i-1) == '0') && (i+1>=str.length() || str.charAt(i+1) == '0')){
+                        count++;
+                        i++;
+                    }
                 }
             }
+            return count;
         }
-        return count;
+        else{
+            return -1;
+        }
+
     }
 
     public static void main(String args[]){
         String choice;
         urinals u = new urinals();
         System.out.println(u.goodString("11001"));
-        System.out.println(u.countUrinals("10000"));
+        System.out.println(u.countUrinals("011"));
     }
 }
 
