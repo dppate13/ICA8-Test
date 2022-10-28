b5699039c7e3a425c348f4a5b5084048a2d75310
dppate13 <dppate13@asu.edu>
2022-10-27 19:20:52 -0700

    add countUriinal function

--- urinals.java
+++ urinals.java
@@ -11,13 +11,25 @@ public class urinals {
      return true;
     }
 
+    int countUrinals(String str){
+        int count = 0;
 
+        for(int i =0; i<str.length();i++){
+
+            if(str.charAt(i) == '0'){
+                if(str.charAt(i+1) == '0' && str.charAt(i-1) == '0'){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
     public static void main(String args[]){
         String choice;
         urinals u = new urinals();
         System.out.println(u.goodString("11001"));
-
+        System.out.println(u.countUrinals("10000"));
     }
 }
 
