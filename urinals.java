347dc9425797da56cf21c83b63264b8edf2107db
dppate13 <dppate13@asu.edu>
2022-10-27 22:18:25 -0700

    switch case

--- urinals.java
+++ urinals.java
@@ -3,6 +3,31 @@ import java.io.*;
 
 public class urinals {
 
+
+    public void getInputKeyboard(){
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.nextLine() != "-1"){
+            String str = String.valueOf(scanner.hasNext());
+            System.out.println(str + "ayu");
+            goodString(str);
+        }
+    }
+
+    public void openFile(){
+        try {
+            File myObj = new File("urinals.dat");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(countUrinals(data));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
     Boolean goodString(String str){
      for(int i =1 ;i< str.length(); i++){
          if(str.charAt(i) == '1' && str.charAt(i-1) == '1'){
@@ -34,19 +59,30 @@ public class urinals {
     }
 
     public static void main(String args[]){
-        String choice;
+
         urinals u = new urinals();
-        try {
-            File myObj = new File("urinals.dat");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(u.countUrinals(data));
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+
+        System.out.println("Choice one option for input:");
+        System.out.println("i. Enter '1' to get input from keyboard.");
+        System.out.println("ii. Enter '2' to get input from file.");
+        System.out.print("Enter your choice = ");
+        Scanner scanner = new Scanner(System.in);
+        int InputType = scanner.nextInt();
+
+
+        switch (InputType) {
+            case 1:
+                System.out.print("Enter input = ");
+                String str = scanner.next();
+                while(!str.equals("-1")){
+                    System.out.println("Max urinals possible: " + u.countUrinals(str));
+                    System.out.print("Enter '-1' to end input or string to continue enter = ");
+                    str = scanner.next();
+                }
+                break;
+            case 2:
+                u.openFile();
+                break;
         }
     }
 }
